87249ff038a675fcec70c30e26095767d6889683
paaco <paaco@users.noreply.github.com>
2019-11-08 16:29:49 +0100

    Fix overflow check using bcc

--- astro.asm
+++ astro.asm
@@ -1,20 +1,31 @@
 ;
 ; astro
 ;
+; aay64.txt VIC PAL/NTSC Differences indicates PAL 19656 cycles/frame (63/line) or 17095 NTSC
 
 SCREEN=$0400
-ENDSCREEN=$0400+7*40
+ENDSCREEN=$0400+25*40
 
     * = $0801
-    !byte $0c,$08,$b5,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
+    !byte $0c,$08,<2019,>2019,$9e,$20,$32,$30,$36,$32,$00,$00,$00
 start:
+            ; cls
+            lda #32
+            ldx #0
+.cls        sta SCREEN,x
+            sta SCREEN+$100,x
+            sta SCREEN+$200,x
+            sta SCREEN+$300,x
+            inx
+            bne .cls
+
             lda #<SCREEN
             sta $fb
             lda #>SCREEN
             sta $fc
 
-            ldx #100 ; count
-volgende:
+            ldx #200 ; count
+next:
             ; determine next star
             clc
             lda $fb
@@ -24,29 +35,29 @@ volgende:
             inc $fc
 .ok:
             ; check overflow
-            ; if $FB/$FC >ENDSCREEN, reduce by (ENDSCREEN-SCREEN)
+            ; if $FB/$FC > ENDSCREEN, reduce by (ENDSCREEN-SCREEN)
             lda $fc
             cmp #>ENDSCREEN
-            bmi .no_overflow
+            bcc .lessthan
+            bne .greater
             lda $fb
             cmp #<ENDSCREEN
-            bmi .no_overflow
-            ; reduce $FB/$FC by (ENDSCREEN-SCREEN)
-            lda $fb
-            sec
+            bcc .lessthan
+.greater:   ; reduce $FB/$FC by (ENDSCREEN-SCREEN)
+            ;lda $fb  already so
+            ;sec      already so
             sbc #<(ENDSCREEN-SCREEN)
             sta $fb
             lda $fc
             sbc #>(ENDSCREEN-SCREEN)
             sta $fc
-
-.no_overflow:
-plotster:
+.lessthan:
+plotstar:
             ;lda #42         ; star
             txa
             ldy #0
             sta ($fb),y
 
             dex
-            bne volgende
+            bne next
             rts
