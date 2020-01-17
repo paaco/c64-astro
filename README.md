45498ed28b8736da073fdea929d7f5944fcee601
paaco <paaco@users.noreply.github.com>
2020-01-17 15:26:13 +0100

    Updated algorithm

--- README.md
+++ README.md
@@ -8,3 +8,30 @@ The star field is made up from different layers of stars spread out randomly ove
 Each layer moves with a slightly different speed to create a parallax effect.
 
 Stars are always plot in the same order, however they wrap around (modulo) offset 1000 ($03e8). This uses 16-bit unsigned compares with cmp, sbc and bcc for "less than".
+
+Unfortunately this trick doesn't work if you also want to scroll sideways.
+Stars will wrap around in that case and that looks ugly.
+
+So the approach is to plot a layer from top to bottom, starting with an offset star (scrolling up) and adding an offset to X-position (scrolling right)
+
+    ; just kept for reference on 16-bit calculations
+    ;SCREEN=$0400
+    ;ENDSCREEN=$0400+25*40
+    ;             ; check overflow
+    ;             ; if $FB/$FC > ENDSCREEN, reduce by (ENDSCREEN-SCREEN)
+    ;             lda $fc
+    ;             cmp #>ENDSCREEN
+    ;             bcc .lessthan
+    ;             bne .greater
+    ;             lda $fb
+    ;             cmp #<ENDSCREEN
+    ;             bcc .lessthan
+    ; .greater:   ; reduce $FB/$FC by (ENDSCREEN-SCREEN)
+    ;             ;lda $fb  already so
+    ;             ;sec      already so
+    ;             sbc #<(ENDSCREEN-SCREEN)
+    ;             sta $fb
+    ;             lda $fc
+    ;             sbc #>(ENDSCREEN-SCREEN)
+    ;             sta $fc
+    ; .lessthan:
--- astro.asm
+++ astro.asm
@@ -3,68 +3,77 @@
 ;
 ; aay64.txt VIC PAL/NTSC Differences indicates PAL 19656 cycles/frame (63/line) or 17095 NTSC
 
+!addr {
 SCREEN=$0400
 ENDSCREEN=$0400+25*40
 
+; variables
+scrptr = $fb
+count = $fd
+xoff = $fe
+}
+
     * = $0801
-    !byte $0c,$08,<2019,>2019,$9e,$20,$32,$30,$36,$32,$00,$00,$00
+    !byte $0c,$08,<2020,>2020,$9e,$20,$32,$30,$36,$32,$00,$00,$00
 start:
             ; cls
             lda #32
             ldx #0
-.cls        sta SCREEN,x
+-           sta SCREEN,x
             sta SCREEN+$100,x
             sta SCREEN+$200,x
             sta SCREEN+$300,x
             inx
-            bne .cls
+            bne -
 
-            lda #<SCREEN
-            sta $fb
-            lda #>SCREEN
-            sta $fc
-            lda #42 ; star
-            sta $fd
+            ldx #0 ; 0..24 increase to scroll up
+            ldy #0 ; 0..39 increase to scroll right
+            jsr drawlayer
 
-            ldx #200 ; count
-next:
-            ; determine next star
-            clc
-            lda $fb
-            adc #47 ; TODO real next star
-            sta $fb
-            bcc .ok
-            inc $fc
-.ok:
-            ; check overflow
-            ; if $FB/$FC > ENDSCREEN, reduce by (ENDSCREEN-SCREEN)
-            lda $fc
-            cmp #>ENDSCREEN
-            bcc .lessthan
-            bne .greater
-            lda $fb
-            cmp #<ENDSCREEN
-            bcc .lessthan
-.greater:   ; reduce $FB/$FC by (ENDSCREEN-SCREEN)
-            ;lda $fb  already so
-            ;sec      already so
-            sbc #<(ENDSCREEN-SCREEN)
-            sta $fb
-            lda $fc
-            sbc #>(ENDSCREEN-SCREEN)
-            sta $fc
-.lessthan:
-plotstar:
-            ldy #0
-            lda ($fb),y
-            ora $fd ; add star (first layer is sta only)
-            sta ($fb),y
+            ldx #7 ; 0..24 increase to scroll up
+            ldy #26 ; 0..39 increase to scroll right
+            jsr drawlayer
 
-            dex
-            bne next
+            ldx #18
+            ldy #4
+            jsr drawlayer
             rts
-;13295 bij 200 stars incl cls
-;14295 met lda+ora ipv lda
 
-; TODO: can you use sbc directly instead of cmp first? low-byte stays the same
-; TODO: can you fill screen and plot bottom layer star field without checking overflow each byte?
+; X = y-scroll (0..24, values scroll up)
+; Y = x-scroll (0..39, values scroll right)
+drawlayer:
+            stx count
+            sty xoff
+            lda #<SCREEN
+            sta scrptr
+            lda #>SCREEN
+            sta scrptr+1
+.nextstar:
+            clc
+            lda layer0,x
+            and #63
+            adc xoff
+            cmp #40
+            bcc +
+            sbc #40
++           tay
+            lda #42 ; star
+            sta (scrptr),y
+            ; next line
+            ;clc ;cleared by sbc
+            lda scrptr
+            adc #40
+            sta scrptr
+            bcc +
+            inc scrptr+1
+            ; next star
++           inx
+            cpx #25
+            bne +
+            ldx #0
++           cpx count
+            bne .nextstar
+            rts
+
+; each layer of the starfield consists of 25 stars, one per screen line on a random x-position
+layer0:     !byte 18,12,26,16,21,31,35,31,3,4,10,30,27,31,27,18,2,26,38,6,29,21,14,23,8
