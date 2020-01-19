6abb46ba6a26d21f3533da9661169407b81a97b3
paaco <paaco@users.noreply.github.com>
2020-01-19 09:51:15 +0100

    Some speedup, thoughts on improve algorithm

--- astro.asm
+++ astro.asm
@@ -27,40 +27,45 @@ start:
             bne -
 
             ldx #0 ; 0..24 increase to scroll up
-            ldy #0 ; 0..39 increase to scroll right
+            ldy #2 ; 0..39 increase to scroll right
             jsr drawlayer
 
-            ldx #7 ; 0..24 increase to scroll up
-            ldy #26 ; 0..39 increase to scroll right
-            jsr drawlayer
+            ; ldx #7 ; 0..24 increase to scroll up
+            ; ldy #26 ; 0..39 increase to scroll right
+            ; jsr drawlayer
 
-            ldx #18
-            ldy #4
-            jsr drawlayer
+            ; ldx #18
+            ; ldy #4
+            ; jsr drawlayer
             rts
 
 ; X = y-scroll (0..24, values scroll up)
 ; Y = x-scroll (0..39, values scroll right)
 drawlayer:
             stx count
+            dey                 ; to correct for C=1 from 'sec' later
             sty xoff
             lda #<SCREEN
             sta scrptr
             lda #>SCREEN
             sta scrptr+1
+            sec                 ; now loop is always entered with C=1
 .nextstar:
-            clc
+            ; Y = A = layer0[x] + xoff
             lda layer0,x
-            and #63
-            adc xoff
-            cmp #40
+            adc xoff            ; sets C=0
+            tay
+            ; if Y >= 40 then Y = A - 40
+            sbc #40-1           ; 1 to correct for C=0
             bcc +
-            sbc #40
-+           tay
+            clc
+            tay
++
+            ; plot star
             lda #42 ; star
             sta (scrptr),y
+
             ; next line
-            ;clc ;cleared by sbc
             lda scrptr
             adc #40
             sta scrptr
@@ -77,3 +82,10 @@ drawlayer:
 
 ; each layer of the starfield consists of 25 stars, one per screen line on a random x-position
 layer0:     !byte 18,12,26,16,21,31,35,31,3,4,10,30,27,31,27,18,2,26,38,6,29,21,14,23,8
+
+;TODO improvements:
+;TODO - modulo40 table
+;TODO - double layer0 to allow for rollover (no need to reset counter)
+;TODO - inline layers into a single loop
+;TODO - plot a star with lda, ora, sta
+;TODO - calculate star characters
